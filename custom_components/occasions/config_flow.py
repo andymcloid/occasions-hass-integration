f0a7bce14df7cbc50e234a34467081d0660852d6
aah <andreas.ahlfeldt@hantverksdata.se>
2024-10-10 16:26:29 +0200

    1.1.91

--- custom_components/occasions/config_flow.py
+++ custom_components/occasions/config_flow.py
@@ -1,49 +1,39 @@
-"""
-    Occasions custom component - (c) Andreas Åhlfeldt
-"""
-
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers import entity_registry
-from homeassistant.const import CONF_NAME
 import homeassistant.helpers.config_validation as cv
 from datetime import datetime
+
 from .const import DOMAIN  # Import your domain
 
 class OccasionsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is not None:
-            # Create an entry if user input exists
-            return self.async_create_entry(
-                title=user_input["name"],
-                data=user_input
-            )
-        
-       # Fetch the list of person entities to populate the person picker
+            return self.async_create_entry(title=user_input["name"], data=user_input)
+
+        # Fetch the list of person entities for the entity picker
         person_entities = await self._get_person_entities()
 
         # Define the schema for the form
         schema = vol.Schema({
-            vol.Required("name", default="Occasion"): str,   # Name input as string
+            vol.Required("name", default="My Occasion"): str,   # Name input
             vol.Required("date", default=datetime.now()): vol.Datetime,  # Date picker
-            vol.Optional("person", default=None): vol.In(person_entities),  # Entity picker for person
+            vol.Optional("person", default=None): vol.In(person_entities),  # Entity picker for person entities
             vol.Optional("icon", default="mdi:calendar"): vol.All(str, vol.Match(r'^mdi:.+')),  # Icon input with validation
         })
 
-        # Show the form for user input
         return self.async_show_form(
             step_id="user",
             data_schema=schema
         )
-    
 
     @callback
     async def _get_person_entities(self):
         """Get a list of person entities."""
-        # Get all entity ids of type 'person'
-        entity_reg = await self.hass.helpers.entity_registry.async_get_registry()
+        entity_reg = entity_registry.async_get(self.hass)
         return [entity.entity_id for entity in entity_reg.entities.values() if entity.entity_id.startswith("person.")]
